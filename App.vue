d1a2497c0b54e39460d86e090cc3b6b9b930f678
Mju6004101362 <68525406+Mju6004101362@users.noreply.github.com>
2020-08-20 16:18:54 +0700

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="D:\the infinity war\theinfinitywar\src\assets\image\anabell.jpg">
+    <infinity msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import infinity from './components/infinity.vue'
+
+export default {
+  name: 'App',
+  components: {
+    infinity
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
